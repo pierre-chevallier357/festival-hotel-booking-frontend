bed755b0f682fdf9f6007a380ba5e6b0a246ffad
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2022-01-27 11:52:59 +0100

    Remove Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { LodgingComponent } from './components/lodging/lodging.component';
 import { FestivalComponent } from './components/festival/festival.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/festival', pathMatch: 'full' },
   { path: 'festival', component: FestivalComponent },
   { path: 'lodging', component: LodgingComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
-  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
